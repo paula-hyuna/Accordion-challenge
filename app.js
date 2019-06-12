34182ced1b39ebf83a371ae82866d3b8d22f1cd6
paula-hyuna <paula.melero.ramirez@hyuna.bb>
2019-06-12 09:21:28 -0400

    Can open and close accordion headers

--- app.js
+++ app.js
@@ -7,43 +7,58 @@ for(var i=0; i<accordionHeader.length; i++) {
 }
 
 ////FUNCTIONS////
+
 function showHideContent(event) {
 
-    var header = event.target;
+    let header = event.target;
     let expanded = header.getAttribute('aria-expanded');
+    let headerContent = header.getAttribute('aria-controls');
 
     if(expanded == 'false') {
         
         header.setAttribute('aria-expanded', 'true');
         
-        let headerContent = header.getAttribute('aria-controls');
-
-        for(var i=0;i<contentSections.length; i++) {
-            //hide content from all other headers and reset attributes
+        var match = false;
+        for(var i=0;i<contentSections.length && !match; i++) {
+            
+                
+            //hide content from expanded headers and reset their attributes
             contentSections[i].setAttribute('aria-hidden', true);
+            
             for(var x=0; x<accordionHeader.length; x++) {
 
                 if(contentSections[i].getAttribute('id') == accordionHeader[x].getAttribute('aria-controls')){
                     accordionHeader[x].setAttribute('aria-expanded', false);
+                    match == true;
+                    break;
                 }
+
             }
             
+            //clicked header should be shown
             if(contentSections[i].id == headerContent) {
                 
                 contentSections[i].setAttribute('aria-hidden', false);
+                header.setAttribute('aria-expanded', true);
                 
             }
         }
     }
     else {
-        //close on click
 
+        //find clicked header's content
+        for (var i = 0; i < contentSections.length; i++) {
 
+            if(contentSections[i].getAttribute('id') === headerContent) {
+                //hide content, reset aria-expanded
+                contentSections[i].setAttribute('aria-hidden', true);
+                header.setAttribute('aria-expanded', false);
+            }
+        }
     }
 
-
-
 }
+
 
 
 

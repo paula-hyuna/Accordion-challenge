1011349d66604b3b3d095b73ef6c868e02509653
paula-hyuna <paula.melero.ramirez@hyuna.bb>
2019-06-12 13:01:51 -0400

    Design changes

--- app.js
+++ app.js
@@ -1,35 +1,39 @@
-var accordionHeader = document.getElementsByClassName('header');
-var contentSections = document.querySelectorAll('[role="region"]');
+const accordionHeader = document.getElementsByClassName('header');
+const contentSections = document.querySelectorAll('[role="region"]');
 
 ////EVENT LISTENERS////
-for(var i=0; i<accordionHeader.length; i++) {
+
+//create an event listener for each button header
+for(let i=0; i<accordionHeader.length; i++) {
     accordionHeader[i].addEventListener('click', showHideContent);
 }
 
-////FUNCTIONS////
+//listen to keyboard events (up and down arrows)
 
+
+////FUNCTIONS////
 function showHideContent(event) {
 
     let header = event.target;
     let expanded = header.getAttribute('aria-expanded');
     let headerContent = header.getAttribute('aria-controls');
+    let buttonIcon = header.children[1];
 
     if(expanded == 'false') {
         
         header.setAttribute('aria-expanded', 'true');
         
-        var match = false;
-        for(var i=0;i<contentSections.length && !match; i++) {
+        for(let i=0;i<contentSections.length; i++) {
             
                 
             //hide content from expanded headers and reset their attributes
             contentSections[i].setAttribute('aria-hidden', true);
             
-            for(var x=0; x<accordionHeader.length; x++) {
+            for(let x=0; x<accordionHeader.length; x++) {
 
                 if(contentSections[i].getAttribute('id') == accordionHeader[x].getAttribute('aria-controls')){
                     accordionHeader[x].setAttribute('aria-expanded', false);
-                    match == true;
+                    accordionHeader[x].children[1].innerHTML = ' &#xe5cf';
                     break;
                 }
 
@@ -40,20 +44,23 @@ function showHideContent(event) {
                 
                 contentSections[i].setAttribute('aria-hidden', false);
                 header.setAttribute('aria-expanded', true);
-                
+                buttonIcon.innerHTML = '&#xe5ce';
+
             }
         }
     }
     else {
 
         //find clicked header's content
-        for (var i = 0; i < contentSections.length; i++) {
+        for (let i = 0; i < contentSections.length; i++) {
 
             if(contentSections[i].getAttribute('id') === headerContent) {
                 //hide content, reset aria-expanded
                 contentSections[i].setAttribute('aria-hidden', true);
                 header.setAttribute('aria-expanded', false);
+                buttonIcon.innerHTML = ' &#xe5cf';
             }
+
         }
     }
 
